de45c5a312a1f421c2569195108a6a8e493a84e6
horakhy <leonidas51000@hotmail.com>
2022-07-13 21:38:40 -0300

    Table and red button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import { Header } from "./components/header/header";
 import { NotificationButton } from "./components/notification-button/notification-button";
+import { SalesCard } from "./components/sales-card/sales-card";
 
 function App() {
   return (
     <>
       <Header />
-      <NotificationButton />
+     <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+        <SalesCard />
+        </div>
+        </section>
+    </main>
     </>
-  );
+  )
 }
 
 export default App;
